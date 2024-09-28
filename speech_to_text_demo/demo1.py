6515f3d9fbd4245abbdb76a7e2468c24db8ab3f7
sing27 <144003114+sing27@users.noreply.github.com>
2024-09-28 18:50:14 +0000

    Add for loop find best confidence respond transcript

--- speech_to_text_demo/demo1.py
+++ speech_to_text_demo/demo1.py
@@ -8,29 +8,47 @@ client_file = 'SLL_demo.json'
 # 傳遞客戶端文件路徑來創建credentail對象
 credentials = service_account.Credentials.from_service_account_file(client_file)
 
-# 當創建語音客戶端對象時，能通過credentail參數來驗証連接或應用程序 在這裡，我們的客戶端名稱是client
-client = speech.SpeechAsyncClient(credentials=credentials)
+# 當創建語音客戶端對象時，能通過credentail參數來驗証連接或應用程序 在這裡的客戶端名稱是client
+client = speech.SpeechClient(credentials=credentials)
 
 # load the audio file 以後應該會改成變數
 # with io.open(audio_file, 'rb') as f:：以二进制读取模式 ('rb') 打开指定的音频文件。with 语句确保文件在使用完毕后自动关闭。
 audio_file = 'japan.wav'
 with io.open(audio_file, 'rb') as f:
     content = f.read()
-    audio = speech.RecognitionAudio(content=content)
+audio = speech.RecognitionAudio(content=content)
 
-# 宣告音頻metadata / Set language_code='und' (for "undetermined")  D: 'en-US'
-#
-config = speech.RecognitionConfig(
-    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=44100,
-    language_code='und',
-)
+# 宣告音頻metadata / Set language_code='und' (for "undetermined")  D: 'en-US' 'ja-JP' 'yue-Hant-HK' 'cmn-Hans-CN'
+# for loop find the best confidence !!! 越多language_codes 越多api
+language_codes = ['yue-Hant-HK', 'ja-JP']
+best_confidence = 0
+best_transcript = ''
+best_language = ''
 
-response = client.recognize(config=config, audio=audio)
+for language_code in language_codes:
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=48000,
+        language_code=language_code,
+        model='default'
+    )
+    response = client.recognize(config=config, audio=audio)
 
-print(response)
+    for result in response.results:
+        confidence = result.alternatives[0].confidence
+        if confidence > best_confidence:
+            best_confidence = confidence
+            best_transcript = result.alternatives[0].transcript
+            best_language = language_code
+
+print(f"Best Transcript ({best_language}): {best_transcript} (Confidence: {best_confidence})")
+#Output: Best Transcript (ja-JP): 桜... (Confidence: 0.9265390038490295)
+
+#    print(f"Results for {language_code}:")
+#    print(response)
 
 """for result in response.results:
     print('Transcript: {}'.format(result.alternatives[0].transcript))
     print('Confidence: {}'.format(result.alternatives[0].confidence))"""
 
+STToutput = best_transcript
