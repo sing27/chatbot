5d9893adb45c465195c0e0307eca4bcf56194a82
sing27 <144003114+sing27@users.noreply.github.com>
2024-09-28 16:59:42 +0000

    to wav?

---
+++ conver_to_wav/Recorder.js
@@ -0,0 +1,18 @@
+var toWav = require('audiobuffer-to-wav')
+var xhr = require('xhr')
+var context = new AudioContext()
+ 
+// request the MP3 as binary
+xhr({
+  uri: 'audio/japan.m4a',
+  responseType: 'arraybuffer'
+}, function (err, body, resp) {
+  if (err) throw err
+  // decode the MP3 into an AudioBuffer
+  audioContext.decodeAudioData(resp, function (buffer) {
+    // encode AudioBuffer to WAV
+    var wav = toWav(buffer)
+    
+    // do something with the WAV ArrayBuffer ...
+  })
+})
